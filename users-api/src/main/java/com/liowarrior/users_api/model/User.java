8e04331a802a46ca24332a0f29120f225f429683
Damian Andres Mejia Novoa <damianmejia029@gmail.com>
2026-09-07 21:41:44 -0500

    Columns - definicion de atributos con GenerateValue (increment) y Enumerated (ENUM) para definir userRole

--- users-api/src/main/java/com/liowarrior/users_api/model/User.java
+++ users-api/src/main/java/com/liowarrior/users_api/model/User.java
@@ -12,9 +12,27 @@ public class User {
 
     //@Id - Marca la llave primaria
     @id
+
     //@GenerateValue - Valor incremental de la DB (AUTO_INCREMENT)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_user")
     private Integer idUser;
 
+    //@Column - Mapear el atributo Java al nombre real de DB
+    @Column(name = "user_name", nullable = false, length = 50)
+    private String userName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    //@Enumerated - Dice a JPA que esto es un ENUM y que lo guarde como texto
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private userRole userRole;
+
+
+}
 
