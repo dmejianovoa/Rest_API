f5d35baf910cd2a1a370fa24f92544352f78d039
Damian Andres Mejia Novoa <damianmejia029@gmail.com>
2026-09-07 21:15:00 -0500

    CDefinir Entity / Table / Id - Atributo id_user

--- users-api/src/main/java/com/liowarrior/users_api/model/User.java
+++ users-api/src/main/java/com/liowarrior/users_api/model/User.java
@@ -1,2 +1,20 @@
+package com.liowarrior.users_api.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+//@Entity - Habla con spring y le dice que va a mapear una BD
+@Entity
+//@Table - Especifica el nombre de la tabla
+@Table(name="Users")
+public class User {
+
+    //@Id - Marca la llave primaria
+    @id
+    //@GenerateValue - Valor incremental de la DB (AUTO_INCREMENT)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer idUser;
 
 
