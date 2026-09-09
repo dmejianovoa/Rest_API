abeb005cc5941ad2aea064746fa2dfcb74e63389
Damian Andres Mejia Novoa <damianmejia029@gmail.com>
2026-09-08 22:44:17 -0500

    Conexion con Base de datos - prueba de compilacion falied por minuscula en id -> Id

--- users-api/src/main/java/com/liowarrior/users_api/model/User.java
+++ users-api/src/main/java/com/liowarrior/users_api/model/User.java
@@ -1,8 +1,6 @@
 package com.liowarrior.users_api.model;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 //@Entity - Habla con spring y le dice que va a mapear una BD
 @Entity
@@ -11,8 +9,7 @@ import org.springframework.boot.autoconfigure.web.WebProperties;
 public class User {
 
     //@Id - Marca la llave primaria
-    @id
-
+    @Id
     //@GenerateValue - Valor incremental de la DB (AUTO_INCREMENT)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_user")
